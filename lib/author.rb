d36af2cf58000f32baa7054fec8d00ab58bd73fc
Dennis Gonzalez <gonzalezdennis6@gmail.com>
2017-12-14 17:17:56 +0000

    automatically backed up by learn

--- lib/author.rb
+++ lib/author.rb
@@ -2,6 +2,8 @@ class Author
 
   attr_accessor :name
 
+@@post_count = 0
+
   def initialize(name)
     @name = name
     @posts = []
@@ -14,7 +16,16 @@ class Author
   def add_post(post)
     @post << post
     post.author = self
+    @@post_count += 1
   end
 
+  def add_post_by_title(title)
+    post = Post.new(title)
+    add_post(post)
+  end
+
+  def self.post_count
+    @@post_count
+  end
 
 end
