1ddc16731c7df06bb64f992266f676a323a9f551
Dennis Gonzalez <gonzalezdennis6@gmail.com>
2017-12-14 16:05:03 +0000

    automatically backed up by learn

---
+++ lib/artist.rb
@@ -0,0 +1,30 @@
+class Artist
+
+  attr_accessor :name
+
+  @@song_count = 0
+  
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+
+  def add_song(song)
+    @songs << song
+    song.self.class = self
+  end
+
+  def add_song_by_name(song)
+    song = song.new
+    song.self.class = self
+  end
+
+  def self.song_count
+    @@song_count
+  end
+    
+    
+
+
+
+end 
